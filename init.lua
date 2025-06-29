db65c1a451da0bfa307b5d4ab838ea3c60d285c1
Terry Pan <pttdev123@gmail.com>
2025-06-30 00:20:28 +1000

    feat: update

--- init.lua
+++ init.lua
@@ -280,6 +280,25 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
+  --[[
+    mx              Set mark x
+    m,              Set the next available alphabetical (lowercase) mark
+    m;              Toggle the next available mark at the current line
+    dmx             Delete mark x
+    dm-             Delete all marks on the current line
+    dm<space>       Delete all marks in the current buffer
+    m]              Move to next mark
+    m[              Move to previous mark
+    m:              Preview mark. This will prompt you for a specific mark to
+                    preview; press <cr> to preview the next mark.
+    m[0-9]          Add a bookmark from bookmark group[0-9].
+    dm[0-9]         Delete all bookmarks from bookmark group[0-9].
+    m}              Move to the next bookmark having the same type as the bookmark under
+                    the cursor. Works across buffers.
+    m{              Move to the previous bookmark having the same type as the bookmark under
+                    the cursor. Works across buffers.
+    dm=             Delete the bookmark under the cursor.
+  --]]
   {
     'chentoast/marks.nvim',
     event = 'VeryLazy',
