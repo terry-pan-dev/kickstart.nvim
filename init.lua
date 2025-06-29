70b1c951108f0e135676c5f74a0b64f716b9f736
Terry Pan <pttdev123@gmail.com>
2025-06-30 00:31:54 +1000

    feat: update

--- init.lua
+++ init.lua
@@ -671,14 +671,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>sl', builtin.colorscheme, { desc = '[S]earch Colorscheme' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S] Find Vim Commands' })
       vim.keymap.set('n', '<leader>ve', '<cmd>edit $MYVIMRC<cr>', { desc = 'Edit Vim' })
-      vim.keymap.set('n', '<leader>vr', '<cmd>source $MYVIMRC<cr>', { desc = 'Reload Vimrc' })
-      vim.keymap.set('n', '<leader>vs', '<cmd>Lazy sync<cr>', { desc = 'Lazy Sync' })
-      vim.keymap.set('n', '<leader>vu', '<cmd>Lazy update<cr>', { desc = 'Lazy Update' })
-      vim.keymap.set('n', '<leader>vl', '<cmd>Lazy reload<cr>', { desc = 'Lazy Reload' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
